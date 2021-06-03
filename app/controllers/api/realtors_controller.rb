06ed153e8f8d05c7593b1bbccea6341435f7aa92
landon hickman <landonhickman12@gmail.com>
2021-06-03 14:47:38 -0600

    realtor backend works

--- app/controllers/api/realtors_controller.rb
+++ app/controllers/api/realtors_controller.rb
@@ -1,16 +1,42 @@
 class Api::RealtorsController < ApplicationController
+  before_action :set_realtor, only: [:show, :update, :destroy]
+
   def index
+    render json: Realtor.all
   end
 
   def show
+    render json: @realtor
   end
 
   def create
+    @realtor = Realtor.new(realtor_params)
+    if(@realtor.save)
+      render json: @realtor
+    else
+      render json: @realtor.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if(@realtor.update(realtor_params))
+      render json: @realtor
+    else
+      render json: @realtor.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    render json: @realtor.destroy
+  end
+
+  private
+
+  def realtor_params
+    params.require(:realtor).permit(:name, :license, :rating)
+  end
+
+  def set_realtor
+    @realtor = Realtor.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'buyers/index'
-    get 'buyers/show'
-    get 'buyers/create'
-    get 'buyers/update'
-    get 'buyers/destroy'
-  end
-  namespace :api do
-    get 'homes/index'
-    get 'homes/show'
-    get 'homes/create'
-    get 'homes/update'
-    get 'homes/destroy'
-  end
-  namespace :api do
-    get 'realtors/index'
-    get 'realtors/show'
-    get 'realtors/create'
-    get 'realtors/update'
-    get 'realtors/destroy'
+    resources :realtors
+    resources :homes do
+      resources :buyers
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  namespace :api do
-    get '/tests', to: 'tests#index'
-  end
 end
